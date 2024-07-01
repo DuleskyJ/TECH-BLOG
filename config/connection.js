270550f1fc8891695f51aa0c9fc141ecc13031f1
Laptop <jdkyaz@gmail.com>
2024-07-01 01:31:14 -0700

    Fix bcrypt issue and update dependencies

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,9 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: process.env.DB_HOST,
       dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
     });
 
 module.exports = sequelize;
