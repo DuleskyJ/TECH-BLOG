613d572563d29eeab59c706db76c28c59b3b259e
Laptop <jdkyaz@gmail.com>
2024-07-01 00:54:49 -0700

    New seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,25 +8,18 @@ const commentData = require('./commentData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  await Post.bulkCreate(postData, {
+    returning: true,
+  });
 
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-      post_id: posts[Math.floor(Math.random() * posts.length)].id,
-    });
-  }
+  await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
