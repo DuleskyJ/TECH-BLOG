3e26bdf6612901c66ee868b9228523b1361f1035
Laptop <jdkyaz@gmail.com>
2024-07-01 04:29:46 -0700

    correcting routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,19 +1,35 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// Login route
+// User registration route
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// User login route
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
+
     if (!userData) {
-      console.log('No user found');
       res.status(400).json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
+
     if (!validPassword) {
-      console.log('Invalid password');
       res.status(400).json({ message: 'Incorrect username or password, please try again' });
       return;
     }
@@ -21,17 +37,16 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      console.log('User logged in successfully');
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
-    console.error(err);
     res.status(400).json(err);
   }
 });
 
-// Logout route
+// User logout route
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -40,12 +55,6 @@ router.post('/logout', (req, res) => {
   } else {
     res.status(404).end();
   }
-});
-
-req.session.save(() => {
-  req.session.user_id = userData.id;
-  req.session.logged_in = true;
-  res.json({ user: userData, message: 'You are now logged in!' });
 });
 
 module.exports = router;
